a177c3b188b9bebdacc1ffa8db554899c3d2179d
sunsh1ne2k <tqluc01012000@gmail.com>
2022-06-06 00:32:38 +0700

    create new model catalog/Language

--- mozilla_local_library/catalog/models.py
+++ mozilla_local_library/catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.conf.global_settings import LANGUAGES as py_languages
 import uuid  # for creating unique key
 
 # Create your models here.
@@ -25,6 +26,8 @@ class Book(models.Model):
     # a genre can contain many books, books can cover many genres.
     genres = models.ManyToManyField(
         "Genre", help_text="Select a genre for this book.")
+    language = models.ForeignKey(
+        'Language', null=True, blank=False, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title
@@ -73,3 +76,8 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=10, null=True,
+                            blank=False, choices=py_languages)
